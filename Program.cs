24652f62c7da0edde3015def48238e25002eb5da
DairokuHiva <d_hiva@hotmail.com>
2022-12-27 11:08:25 +0900

    WB_First

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WildBaseProject1
+{
+    public class WBp1
+    {
+        public static void Main ( string [ ] args )
+        {
+            var mmm = new WBp1();
+            mmm.ClsMain();
+        }
+        internal void ClsMain ( )
+        {
+            var msg = "Checks";
+            Console.WriteLine($"{msg}");
+        }
+    }
+}
